d9356cdef10ac6ebd7bb3cf2cbfb115ef115ff6b
DEV <DEV@A21PC16>
2025-11-19 17:31:56 +0200

    fixed arrays

--- 02_P/base.js
+++ 02_P/base.js
@@ -6,11 +6,13 @@ for(let i = 0; i < 10; i++) {
 alert("Generated array: " + array);
 
 //2
-array.array.forEach((el, index) => {
+console.log("Array elements with their indices:");
+array.forEach((el, index) => {
     console.log(`[${index}] - ${el}`);
 });
 
 //3
+console.log("Elements divisible by 7:");
 let flag = false;
 for(value of array){
     if(value % 7 === 0)
@@ -24,15 +26,19 @@ if(!flag){
 }
 
 //4
+console.log("Array sorted in descending order:");
 array.sort((a, b) => b-a);
 
 //5
-array.splice(array.length/2,array.length, 0);
+console.log("Inserting 0s in the middle of the array:");
+array.splice(array.length/2,array.length, 0,0,0,0,0);
 
 //6
+console.log("Removing first three elements of the array:");
 array.shift(3);
 
 //7
+console.log("Finding pairs of equal elements in the array:");
 for(let i = 0; i < array.length; i++){
     for(let j = i + 1; j < array.length; j++){
         if(array[i] == array[j]){
@@ -43,12 +49,19 @@ for(let i = 0; i < array.length; i++){
 
 
 //8
+console.log("Creating a new array without the first and last elements:");
 let an_arr = array.slice(1, array.length - 1);
 
+an_arr.forEach((el, index) => {
+    console.log(`[${index}] - ${el}`);
+});
+
 //9
+
 let count = 0;
 for(let value of array){
     if(value % 2 === 0){
         count++;
     }
 }
+console.log(`Amount of even numbers in the "array" is: ${count}`);
