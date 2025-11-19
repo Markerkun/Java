519c3d1b1a0d494175d8e1384344c7a47ff87303
DEV <DEV@A21PC16>
2025-11-19 17:53:27 +0200

    string

---
+++ 02_H/base.js
@@ -0,0 +1,17 @@
+//1
+let str = prompt("Write down your string")
+
+let count_space;
+for(let i = 0; i < 10; i++) {
+    if(str[i]==' ')
+    {
+        count_space++;
+    }
+}
+console.log(`Spaces in that string: ${count_space}`);
+
+//2
+function Capitalize(string)
+{
+    
+}
