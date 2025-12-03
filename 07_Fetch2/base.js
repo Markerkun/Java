41de15e8114fa8a004bb12e7d602299ca49a39d6
DEV <DEV@A21PC16>
2025-12-03 17:25:26 +0200

    +dogs

--- 07_Fetch2/base.js
+++ 07_Fetch2/base.js
@@ -6,12 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function SomeNewF(){
     const url = "https://dog.ceo/api/breeds/image/random";
+    let response = await fetch (url);
+    let data = await response.json();
+    const breeds = data.message;
+    const p = document.getElementById("p");
     const img = document.getElementById("DogImg");
     img.hidden = false;
      fetch (url)
      .then (response => response.json())
      .then (data => {img.src = data.message})
-     .catch(error => (console.log (error)));     
+     .catch(error => (console.log (error)));  
+    
+    const imageUrl = data.message;
+    img.src = imageUrl;
 
+    const parts = imageUrl.split("/");
+    const breedPart = parts[parts.indexOf("breeds") + 1];
+
+    const breed = breedPart.replace("-", " ");
+
+    p.textContent = "Breed: " + breed;
+
+
+    
+        
 
 }
