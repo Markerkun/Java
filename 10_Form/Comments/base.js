ca250ad6e7b8923a425be944ba54bbf41c9d05b7
Mark <markmyskovets9@gmail.com>
2025-12-23 20:58:54 +0200

    Forms

--- 10_Form/Comments/base.js
+++ 10_Form/Comments/base.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = document.getElementById("messageForm");
 const messages = document.getElementById("messages");
 
@@ -14,30 +13,3 @@ form.addEventListener("submit", function (e) {
   messages.appendChild(li);
   form.reset();
 });
-=======
-
-
-function AddComment(event){
-    event.preventDefault();
-
-    const Comments = document.getElementById("ListOfCommments");
-
-    const form = event.target;
-    const Name = form["nameInput"].value;
-    const Context = form["TextArea"].value;
-
-
-    Comments.innerHTML += 
-    `<div>
-        <p>
-            Hi, I'm ${Name}.
-            ${Context}
-            ${new Date()}
-        </p>
-    </div>`
-
-
-
-
-}
->>>>>>> 9df5a81eecd534052c01c8cf6b04e4e73211a4e2
